2cade7bd864ff6540445f0aa4772d97fb2fbd8de
Tobias Macey <tmacey@mit.edu>
2017-01-24 09:24:08 -0500

    Fixed course asset backup script
    If the /mnt/efs directory already exists the script was
    failing. Updated to add the `-p` flag to `mkdir`.

--- salt/backups/templates/course_assets.sh
+++ salt/backups/templates/course_assets.sh
@@ -3,7 +3,7 @@ set -e
 
 {% set backupdir = '/mnt/efs/{}'.format(settings.get('directory', 'course_assets')) %}
 
-sudo mkdir /mnt/efs
+sudo mkdir -p /mnt/efs
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).{{ settings.efs_id }}.efs.us-east-1.amazonaws.com:/ /mnt/efs
 
 PASSPHRASE={{ settings.duplicity_passphrase }} /usr/bin/duplicity \
