720477f065e99ccdfcf3563a297f163d525ef466
Tobias Macey <tmacey@mit.edu>
2017-02-09 15:56:05 -0500

    Fixed syntax error when generating date string

--- salt/backups/templates/backup_consul.sh
+++ salt/backups/templates/backup_consul.sh
@@ -3,7 +3,7 @@ set -e
 
 {% set backupdir = '/backups/{}'.format(settings.get('directory', 'consul')) %}
 mkdir -p {{ backupdir }}
-SNAPFILE={{ backupdir }}/consul_snapshot-`$(date +%Y-%m-%d_%H-%M-%S)`.snap
+SNAPFILE={{ backupdir }}/consul_snapshot-`date +%Y-%m-%d_%H-%M-%S`.snap
 
 /usr/local/bin/consul snapshot save -stale -token={{ settings.acl_token }} $SNAPFILE
 
--- salt/backups/templates/backup_elasticsearch.sh
+++ salt/backups/templates/backup_elasticsearch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-SNAPFILE=elasticsearch_snapshot-`$(date +%Y-%m-%d_%H-%M-%S)`
+SNAPFILE=elasticsearch_snapshot-`date +%Y-%m-%d_%H-%M-%S`
 curl -XPUT http://elasticsearch.service.consul:9200/_snapshot/{{ settings.snapshot_repository_name }}/$SNAPFILE?wait_for_completion=true
