e683f0fee5b05162cc795e7cc1dea0f60b70d202
Tobias Macey <tmacey@mit.edu>
2017-02-09 15:56:05 -0500

    Fixed syntax error when generating date string and s3 URL

--- salt/backups/templates/backup_consul.sh
+++ salt/backups/templates/backup_consul.sh
@@ -7,6 +7,6 @@ SNAPFILE={{ backupdir }}/consul_snapshot-`date +%Y-%m-%d_%H-%M-%S`.snap
 
 /usr/local/bin/consul snapshot save -stale -token={{ settings.acl_token }} $SNAPFILE
 
-aws s3 cp $SNAPFILE s3+http://odl-operations-backups/{{ settings.get('directory', 'consul') }}/
+aws s3 cp $SNAPFILE s3://odl-operations-backups/{{ settings.get('directory', 'consul') }}/
 
 rm $SNAPFILE
