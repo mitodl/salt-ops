1732926337de9180c8da76e9ada64f84993f9286
Tobias Macey <tmacey@mit.edu>
2017-05-12 11:00:14 -0400

    Updated restore script for course assets
    Updated the restore script for course assets so that it will execute
    properly by downloading the backed up files and then rsyncing them to
    their final destination.

--- salt/backups/templates/restore_course_assets.sh
+++ salt/backups/templates/restore_course_assets.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-{% set backupdir = '/mnt/efs/{}'.format(settings.get('directory', 'course_assets')) %}
+{% set backupdir = '{}'.format(settings.get('directory', 'course_assets')) %}
 
 mkdir -p /mnt/efs
 mountpoint /mnt/efs || mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).{{ settings.efs_id }}.efs.us-east-1.amazonaws.com:/ /mnt/efs
 
 PASSPHRASE={{ settings.duplicity_passphrase }} /usr/bin/duplicity restore \
-          --s3-use-server-side-encryption s3+http://odl-operations-backups/{{ settings.get('directory', 'course_assets') }}/ \
+          --s3-use-server-side-encryption s3+http://odl-operations-backups/{{ backupdir }}/ \
           --archive-dir /backups/.cache \
-          --force --tempdir /backups {{ backupdir }}/
+          --force --tempdir /backups /backups/{{ backupdir }}_copy/
+
+rsync -auv --delete /backups/{{ backupdir }}_copy/ /mnt/efs/{{ backupdir }}
+rm -rf /backups/{{ backupdir }}_copy
