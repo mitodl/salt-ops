07a352a7aabd9a97518b0521a3de3802e38f8b81
Tobias Macey <tmacey@mit.edu>
2017-07-18 15:56:28 -0400

    Fixed path mismatch for mysql restore

--- salt/backups/templates/restore_mysql.sh
+++ salt/backups/templates/restore_mysql.sh
@@ -9,7 +9,7 @@ mkdir -p {{ cachedir }}
 PASSPHRASE={{ settings.duplicity_passphrase }} /usr/bin/duplicity restore \
           --s3-use-server-side-encryption s3+http://odl-operations-backups/{{ settings.get('directory', 'mysql') }} \
           --archive-dir {{ cachedir }} --s3-use-multiprocessing \
-          --force --tempdir /backups/tmp/ {{ backupdir }}/{{ settings.restore_from }}
+          --force --tempdir /backups/tmp/ {{ backupdir }}/
 
 /usr/bin/mysql --host {{ settings.host }} \
                --port {{ settings.get('port', 3306) }} \
