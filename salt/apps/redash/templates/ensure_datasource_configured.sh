d9a63d2a331da866b37b7e312681e025376a1114
Tobias Macey <tmacey@mit.edu>
2018-04-02 19:47:02 -0400

    Added quoting of names for data sources to manage spaces in name

--- salt/apps/redash/templates/ensure_datasource_configured.sh
+++ salt/apps/redash/templates/ensure_datasource_configured.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 OPTIONS='{{ ds.options|json }}'
-NAME={{ ds.name }}
+NAME="{{ ds.name }}"
 DS_TYPE={{ ds.type }}
 
-RESULT=$(python manage.py ds edit --type $DS_TYPE --options "$OPTIONS" $NAME)
+RESULT=$(python manage.py ds edit --type $DS_TYPE --options "$OPTIONS" "$NAME")
 if [ $(echo $RESULT | grep -v "Couldn't find") ]
 then
     echo "$NAME successfully updated"
 else
-    python manage.py ds new --type $DS_TYPE --options "$OPTIONS" $NAME
+    python manage.py ds new --type $DS_TYPE --options "$OPTIONS" "$NAME"
 fi
