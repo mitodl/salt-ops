880477bde3397b2f3701c8b5ea7dbe4523358702
Tobias Macey <tmacey@mit.edu>
2018-04-04 16:46:32 -0400

    Moved bash interpolation to use Jinja templating instead for quoting reasons

--- salt/apps/redash/templates/ensure_datasource_configured.sh
+++ salt/apps/redash/templates/ensure_datasource_configured.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-OPTIONS='{{ ds.options|json }}'
 NAME="{{ ds.name }}"
 DS_TYPE={{ ds.type }}
 
-RESULT=$(python manage.py ds edit --type $DS_TYPE --options "$OPTIONS" "$NAME")
+RESULT=$(python manage.py ds edit --type $DS_TYPE --options '{{ ds.options|json }}' "$NAME")
 if [ $(echo $RESULT | grep -v "Couldn't find") ]
 then
     echo "$NAME successfully updated"
 else
-    python manage.py ds new --type $DS_TYPE --options "$OPTIONS" "$NAME"
+    python manage.py ds new --type $DS_TYPE --options '{{ ds.options|json }}' "$NAME"
 fi
