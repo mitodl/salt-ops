5d7e6ab668c33432b983c2095bb5232beeb45c56
Tobias Macey <tmacey@mit.edu>
2018-04-04 16:52:15 -0400

    Fixed bash conditional for managing data sources

--- salt/apps/redash/templates/ensure_datasource_configured.sh
+++ salt/apps/redash/templates/ensure_datasource_configured.sh
@@ -4,9 +4,9 @@ NAME="{{ ds.name }}"
 DS_TYPE={{ ds.type }}
 
 RESULT=$(python manage.py ds edit --type $DS_TYPE --options '{{ ds.options|json }}' "$NAME")
-if [ $(echo $RESULT | grep -v "Couldn't find") ]
+if [ $(echo $RESULT | grep "Couldn't find") ]
 then
-    echo "$NAME successfully updated"
-else
     python manage.py ds new --type $DS_TYPE --options '{{ ds.options|json }}' "$NAME"
+else
+    echo "$NAME successfully updated"
 fi
