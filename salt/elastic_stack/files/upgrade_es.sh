f05a92fc20c2019fdbd12414e3c0e0f3558a0572
Mark Breedlove <mbrdlove@mit.edu>
2021-03-25 15:37:18 -0400

    Improve Open Distro upgrade script and orchestration
    - Fix the shell script so that it jumps through more hoops
    - Update the orchestration so that it does a full cluster shutdown

--- salt/elastic_stack/files/upgrade_es.sh
+++ salt/elastic_stack/files/upgrade_es.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-systemctl stop elasticsearch
+systemctl stop elasticsearch || true
 cp -p /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.backup-pre-upgrade
+umount /var/lib/elasticsearch
 dpkg -P elasticsearch
+mkdir /var/lib/elasticsearch
+mount /var/lib/elasticsearch
 grep -v 'artifacts.elastic.co' /etc/apt/sources.list > /tmp/sources.list && mv /tmp/sources.list /etc/apt/sources.list
 dpkg -P ca-certificates-java openjdk-8-jre-headless
 grep -v 'ftp.us.debian.org/debian/ stretch main contrib non-free' /etc/apt/sources.list > /tmp/sources.list && mv /tmp/sources.list /etc/apt/sources.list
@@ -13,6 +16,7 @@ apt-get clean
 apt-get install -y software-properties-common dirmngr apt-transport-https unzip
 echo "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
+apt-get install -y ca-certificates-java  # must happen on its own line, before openjdk-11-jdk
 apt-get install -y openjdk-11-jdk
 wget -qO - https://d3g5vo6xdbdb9a.cloudfront.net/GPG-KEY-opendistroforelasticsearch | apt-key add -
 echo "deb https://d3g5vo6xdbdb9a.cloudfront.net/apt stable main" | tee -a /etc/apt/sources.list.d/opendistroforelasticsearch.list
@@ -23,4 +27,14 @@ dpkg -i elasticsearch-oss-7.10.2-amd64.deb
 rm elasticsearch-oss-7.10.2-amd64.deb
 apt-get install -y opendistro-index-management opendistro-alerting opendistro-job-scheduler
 cp -p /etc/elasticsearch/elasticsearch.yml.backup-pre-upgrade /etc/elasticsearch/elasticsearch.yml
-systemctl start elasticsearch
+find /etc/elasticsearch -nogroup -exec chgrp elasticsearch {} \;
+find /etc/elasticsearch -nouser -exec chown elasticsearch {} \;
+find /var/lib/elasticsearch -nogroup -exec chgrp elasticsearch {} \;
+find /var/lib/elasticsearch -nouser -exec chown elasticsearch {} \;
+find /usr/share/elasticsearch -nogroup -exec chgrp elasticsearch {} \;
+find /usr/share/elasticsearch -nouser -exec chown elasticsearch {} \;
+chown -Rh root:root /var/lib/elasticsearch/lost+found
+/usr/share/elasticsearch/bin/elasticsearch-plugin install -b discovery-ec2
+/usr/share/elasticsearch/bin/elasticsearch-plugin install -b repository-s3
+systemctl daemon-reload
+systemctl enable elasticsearch.service
